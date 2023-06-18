65f351224c5bdb233919693a1fb80806b13c781e
Jonas Roussel <go.jroussel@gmail.com>
2023-06-18 14:06:45 +0200

    fix: reply types

--- example/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { Handler, Is, Schema, SchemaType, Server } from 'typerestjs'
-
-const server = new Server()
-
-server.start()
--- src/validator.ts
+++ src/validator.ts
@@ -3,26 +3,27 @@ import { ZodTypeAny, z } from 'zod'
 export const Is = {
 	...z,
 
+	success: <Z extends ZodTypeAny>(data: Z = z.undefined() as Z) =>
+		z.object({
+			success: z.literal(true),
+			data: data,
+		}),
+
+	error: <Z extends string>(...types: readonly [Z, ...Z[]]) =>
+		z.object({
+			success: z.literal(false),
+			error: z.object({
+				type: z.enum(types),
+				message: z.string(),
+				details: z.array(z.any()).optional(),
+			}),
+		}),
+
 	file: (mimetype?: RegExp) =>
 		z.object({
 			path: z.string(),
 			filename: z.string(),
 			size: z.number(),
 			mimetype: mimetype ? z.string().regex(mimetype) : z.string(),
-		}),
-
-	success: <Z extends ZodTypeAny>(data?: Z) =>
-		z.object({
-			success: z.literal(true),
-			data: data ?? z.void(),
-		}),
-
-	error: <Z extends string>(type: Z) =>
-		z.object({
-			success: z.literal(false),
-			error: z.object({
-				type: z.literal(type),
-				message: z.string(),
-			}),
 		}),
 }
