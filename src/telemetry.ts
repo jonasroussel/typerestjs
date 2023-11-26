535b23b798fb3a590e3e435dad243383d7ebd1de
Jonas Roussel <go.jroussel@gmail.com>
2023-11-27 00:06:05 +0100

    Fix leaking secret

--- src/telemetry.ts
+++ src/telemetry.ts
@@ -12,11 +12,7 @@ const sdk = new NodeSDK({
 	traceExporter: new OTLPTraceExporter({
 		url: 'https://tempo-prod-10-prod-eu-west-2.grafana.net/tempo',
 		timeoutMillis: 5000,
-		headers: {
-			//'726123:glc_eyJvIjoiNzU5MjAyIiwibiI6InN0YWNrLTc3NDAyMy1odC1yZWFkLXRyYWNlcyIsImsiOiIxWTRyMGs3UjRXM2QxZDgycm04WlBDdWciLCJtIjp7InIiOiJwcm9kLWV1LXdlc3QtMiJ9fQ=='
-			Authorization:
-				'Basic NzI2MTIzOmdsY19leUp2SWpvaU56VTVNakF5SWl3aWJpSTZJbk4wWVdOckxUYzNOREF5TXkxb2RDMXlaV0ZrTFhSeVlXTmxjeUlzSW1zaU9pSXhXVFJ5TUdzM1VqUlhNMlF4WkRneWNtMDRXbEJEZFdjaUxDSnRJanA3SW5JaU9pSndjbTlrTFdWMUxYZGxjM1F0TWlKOWZRPT0=',
-		},
+		headers: {},
 	}),
 	// metricReader: new PeriodicExportingMetricReader({
 	// 	exportIntervalMillis: 10_000,
